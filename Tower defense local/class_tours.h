21c7645309aee24ba5de2a42bfba71530b02cc2a
Mathurin RATIE <mathurin.ratie@epsi.fr>
2021-04-17 21:30:15 +0200

    placement des tours

--- Tower defense local/class_tours.h
+++ Tower defense local/class_tours.h
@@ -1,27 +1,29 @@
+#include <SFML/Graphics.hpp>
+
+
 class Canon {
 private:
     int degat_par_coup;
     int portee;
     int coup_par_seconde;
     int level;
-    int cout_init;
-    int cout_2;
-    int cout_3;
+    int cout[3];
+    sf::Vector2i position;
 public:
-    Canon();
+    Canon(int x, int y);
     void level_up();
 };
-
-Canon::Canon() {
+Canon::Canon(int x, int y) {
     this->degat_par_coup = 20;
     this->portee = 100;               //TODO
     this->coup_par_seconde = 5;       //TODO
     this->level = 1;
-    this->cout_init = 100;
-    this->cout_2 = 120;
-    this->cout_3 = 150;
+    this->cout[0] = 100;
+    this->cout[1] = 120;
+    this->cout[2] = 150;
+    this->position.x = x;
+    this->position.y = y;
 }
-
 void Canon::level_up() {
     if (this->level == 1) {
         this->level = 2;
@@ -44,24 +46,23 @@ private:
     int portee;
     int coup_par_seconde;
     int level;
-    int cout_init;
-    int cout_2;
-    int cout_3;
+    int cout[3];
+    sf::Vector2i position;
 public:
-    Canon_Glace();
+    Canon_Glace(int x, int y);
     void level_up();
 };
-
-Canon_Glace::Canon_Glace() {
+Canon_Glace::Canon_Glace(int x, int y) {
     this->degat_par_coup = 10;
     this->portee = 150;               //TODO
     this->coup_par_seconde = 2;       //TODO
     this->level = 1;
-    this->cout_init = 120;
-    this->cout_2 = 150;
-    this->cout_3 = 200;
+    this->cout[0] = 120;
+    this->cout[1] = 150;
+    this->cout[2] = 200;
+    this->position.x = x;
+    this->position.y = y;
 }
-
 void Canon_Glace::level_up() {
     if (this->level == 1) {
         this->level = 2;
@@ -84,24 +85,23 @@ private:
     int portee;
     int coup_par_seconde;
     int level;
-    int cout_init;
-    int cout_2;
-    int cout_3;
+    int cout[3];
+    sf::Vector2i position;
 public:
-    Mortier();
+    Mortier(int x, int y);
     void level_up();
 };
-
-Mortier::Mortier() {
+Mortier::Mortier(int x, int y) {
     this->degat_par_coup = 30;
     this->portee = 150;               //TODO
     this->coup_par_seconde = 2;       //TODO
     this->level = 1;
-    this->cout_init = 150;
-    this->cout_2 = 200;
-    this->cout_3 = 250;
+    this->cout[0] = 150;
+    this->cout[1] = 200;
+    this->cout[2] = 250;
+    this->position.x = x;
+    this->position.y = y;
 }
-
 void Mortier::level_up() {
     if (this->level == 1) {
         this->level = 2;
@@ -125,24 +125,23 @@ private:
     int portee;
     int coup_par_seconde;
     int level;
-    int cout_init;
-    int cout_2;
-    int cout_3;
+    int cout[3];
+    sf::Vector2i position;
 public:
-    Archers();
+    Archers(int x, int y);
     void level_up();
 };
-
-Archers::Archers() {
+Archers::Archers(int x, int y) {
     this->degat_par_coup = 15;
     this->portee = 120;               //TODO
     this->coup_par_seconde = 7;       //TODO
     this->level = 1;
-    this->cout_init = 70;
-    this->cout_2 = 100;
-    this->cout_3 = 120;
+    this->cout[0] = 70;
+    this->cout[1] = 100;
+    this->cout[2] = 120;
+    this->position.x = x;
+    this->position.y = y;
 }
-
 void Archers::level_up() {
     if (this->level == 1) {
         this->level = 2;
